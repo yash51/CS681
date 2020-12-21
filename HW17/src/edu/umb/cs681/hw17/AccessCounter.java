d4d31dce6c8718995c9eede2926747b03a810689
Yash <y.mahant001@umb.edu>
2020-12-21 17:33:05 +0530

    HW17

---
+++ HW17/src/edu/umb/cs681/hw17/AccessCounter.java
@@ -0,0 +1,46 @@
+package edu.umb.cs681.hw17;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AccessCounter { // a thread-safe Singleton class.
+
+	// Define a HashMap<java.nio.file.Path, Integer>
+	// Map<java.nio.file.Path, Integer> hmap = new HashMap<java.nio.file.Path,
+	// Integer>();
+	private ConcurrentHashMap<Path, Integer> hmap = new ConcurrentHashMap<>();
+	// a regular (non-static) lock and use the lock in increment() and getCount()
+	private ReentrantLock lock = new ReentrantLock();
+	// another (static) lock and use the lock in getInstance()
+	private static ReentrantLock staticLock = new ReentrantLock();
+	// instance
+	private static AccessCounter instance = null;
+
+	public static AccessCounter getInstance() {
+		staticLock.lock();
+		try {
+			if (instance == null) {
+				instance = new AccessCounter();
+			}
+		} finally {
+			staticLock.unlock();
+		}
+		return instance;
+	}
+
+	public void increment(Path path) {
+
+		hmap.put(path, hmap.getOrDefault(path, 0) + 1);
+
+	}
+
+	public int getCount(java.nio.file.Path path) {
+
+		return hmap.getOrDefault(path, 0);
+
+	}
+
+}
