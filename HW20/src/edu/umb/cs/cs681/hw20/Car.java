fda6b55fcb21e95a62e6791cd51f297b006ecfbc
Yash <y.mahant001@umb.edu>
2020-12-21 17:35:07 +0530

    HW20

---
+++ HW20/src/edu/umb/cs/cs681/hw20/Car.java
@@ -0,0 +1,163 @@
+package edu.umb.cs.cs681.hw20;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Car {
+
+	private String model, make;
+	private int mileage, year, dominationCount;
+	private int price, gasCapacity;
+
+	public Car(String make, String model, int mileage, int year, int price, int gasCapacity) {
+		super();
+		this.model = model;
+		this.make = make;
+		this.mileage = mileage;
+		this.year = year;
+		this.price = price;
+		this.gasCapacity = gasCapacity;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public int getMileage() {
+		return mileage;
+	}
+
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getDominationCount() {
+		return this.dominationCount;
+	}
+
+	public void setDominationCount(ArrayList<Car> cars) {
+		int count = 0;
+		for (Car car : cars) {
+			if (!car.equals(this)) {
+				float price = car.getPrice();
+				int year = car.getYear();
+				int mileage = car.getMileage();
+
+				if (this.getYear() >= year && this.getMileage() <= mileage && this.getPrice() <= price) {
+					if (this.getYear() > year || this.getMileage() < mileage || this.getPrice() < price) {
+						count++;
+					}
+				}
+			}
+		}
+		;
+		this.dominationCount = count;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public static void setUp(ArrayList<Car> cars) {
+		cars.add(new Car("Tata", "Nano", 200, 2015, 2000, 100));
+		cars.add(new Car("Tesla", "3", 30, 2019, 500, 400));
+		cars.add(new Car("BMW", "Q5", 300, 2012, 20000, 800));
+		cars.add(new Car("Mercedes", "Benz", 0, 2020, 300, 1200));
+		cars.add(new Car("Ford", "Escatic", 10, 2018, 800, 1000));
+
+	}
+
+	public int getGasCapacity() {
+		return gasCapacity;
+	}
+
+	public void setGasCapacity(int gasCapacity) {
+		this.gasCapacity = gasCapacity;
+	}
+
+	static Integer minPrice(ArrayList<Car> cars) {
+		return cars.stream().parallel().map((Car car) -> car.getPrice()).reduce(0, (result, price) -> {
+			if (result == 0)
+				return price;
+			else if (price < result)
+				return price;
+			else
+				return result;
+		});
+	}
+
+	static Integer maxPrice(ArrayList<Car> cars) {
+		return cars.stream().parallel().map((Car car) -> car.getPrice()).reduce(0, (result, carPrice) -> {
+			if (result == 0)
+				return carPrice;
+			else if (carPrice > result)
+				return carPrice;
+			else
+				return result;
+		});
+
+	}
+
+	static Integer totalGasCapacity(ArrayList<Car> cars) {
+		return cars.stream().parallel().map(Car::getGasCapacity).reduce(0, (result, hp) -> result += hp,
+				(finalResult, intermediateResult) -> finalResult + intermediateResult);
+
+	}
+
+	static Integer countOfCars(ArrayList<Car> cars) {
+		return cars.stream().parallel().map((Car car) -> car.getMake()).reduce(0, (result, make) -> ++result,
+				(finalResult, intermediateResult) -> finalResult + intermediateResult);
+	}
+
+	public static void main(String args[]) {
+		ArrayList<Car> cars = new ArrayList<>();
+		setUp(cars);
+
+		cars.forEach((Car car) -> car.setDominationCount(cars));
+
+		Integer minPrice = minPrice(cars);
+
+		System.out.println("Minimum price from cars is: " + minPrice);
+
+		Integer maxPrice = maxPrice(cars);
+
+		System.out.println("Max price from cars is: " + maxPrice);
+
+		Integer numberOfCars = countOfCars(cars);
+
+		System.out.println("Number of cars is: " + numberOfCars);
+
+		Integer totalGasCapacity = totalGasCapacity(cars);
+
+		System.out.println("Number of cars is: " + totalGasCapacity);
+
+	}
+
+}
